9e6acf5a890390cc223705fd6c7e232b98a5e8e0
Bobby Warner <bobbywarner@gmail.com>
2011-09-14 10:22:01 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Grails Ruby
+===
+
+Grails Ruby is a plugin built to use the [JRuby][JRuby] implementation of the Ruby programming language with the [Grails][Grails] framework.
+[Grails]: http://grails.org/
+[JRuby]: http://jruby.org/
+	
+License
+---
+
+Grails Ruby is licensed under the terms of the [Apache License, Version 2.0][Apache License, Version 2.0].
+[Apache License, Version 2.0]: http://www.apache.org/licenses/LICENSE-2.0.html
---
+++ RubyGrailsPlugin.groovy
@@ -0,0 +1,53 @@
+class RubyGrailsPlugin {
+    // the plugin version
+    def version = "1.0.M1"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "2.0 > *"
+    // the other plugins this plugin depends on
+    def dependsOn = [:]
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+        "grails-app/views/error.gsp"
+    ]
+
+    def title = "Ruby Plugin" // Headline display name of the plugin
+    def author = "Bobby Warner"
+    def authorEmail = "bobbywarner@gmail.com"
+    def description = "Plugin for using Ruby code in Grails via JRuby."
+    def documentation = "http://grails.org/plugin/ruby"
+    def license = "APACHE"
+    
+    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPRUBY" ]
+    def scm = [ url: "https://github.com/bobbywarner/grails-ruby" ]
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+
+    def onShutdown = { event ->
+        // TODO Implement code that is executed when the application shuts down (optional)
+    }
+}
