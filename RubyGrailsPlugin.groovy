2074d1582e009547c1a42f7dcabaa0582baa8f88
Bobby Warner <bobbywarner@gmail.com>
2011-09-23 09:26:44 -0500

    changed grailsVersion to allow for 1.2 and newer

--- RubyGrailsPlugin.groovy
+++ RubyGrailsPlugin.groovy
@@ -6,7 +6,7 @@ class RubyGrailsPlugin {
     // the plugin version
     def version = "1.0.M1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0 > *"
+    def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
