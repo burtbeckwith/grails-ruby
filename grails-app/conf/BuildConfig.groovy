667a07fed6236291cf430b8346edb29887807805
Burt Beckwith <burt@burtbeckwith.com>
2011-09-24 12:11:26 -0400

    no need for the Tomcat plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,29 +1,27 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
     inherits("global") {
         excludes 'ehcache'
     }
-    
+
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    
+
     repositories {
         grailsPlugins()
         grailsHome()
         grailsCentral()
         mavenCentral()
     }
-    
+
     dependencies {
         runtime 'org.jruby:jruby:1.6.4'
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:1.0.0.RC3") {
+        build(":release:1.0.0.RC3") {
             export = false
         }
     }
